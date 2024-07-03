5c80b95d71c81e803e506723bb89fd6d112e12fa
Md. Al Abedin Wasee <mdalabedinwasee@gmail.com>
2024-07-03 14:38:58 +0600

    test widget

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,26 @@ class IntroApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.greenAccent,
           title: Text('Home'),
         ),
         body: Center(
-          child: Text('Hello from Wasee'),
+          child: Text('Hello World! I am Wasee from Bangladesh. I live in Dhaka.',
+            textAlign: TextAlign.center,
+            maxLines: 2,
+            style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.amber,
+              decoration: TextDecoration.none,
+              letterSpacing: 1,
+              wordSpacing: 1,
+              overflow: TextOverflow.ellipsis
+            ),
+          ),
         ),
       ),
     );
